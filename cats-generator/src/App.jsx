cc32e54856d4f986c572e12852dddbb006e0a388
Adriel Estoller <adrielestoller@gmail.com>
2025-01-11 10:31:23 -0300

    Add 'Generete cat!' button html and css

--- cats-generator/src/App.jsx
+++ cats-generator/src/App.jsx
@@ -3,9 +3,10 @@ import './App.css'
 function App() {
 
   return (
-    <>
+    <div>
       <h1>Hello World!</h1>
-    </>
+      <button id="catGenerate">Generate cat!</button>
+    </div>
   )
 }
 
